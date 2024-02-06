db66ca92ba83c02165493434c1827695b9d995db
Siddharth <siddharth8271skg2@gmail.com>
2024-02-06 16:42:49 +0530

    introduction to chat and redefining the dashboard layout

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,12 +1,30 @@
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { fetchRedis } from "@/redis/helper";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import { Message } from "postcss";
 
 interface pageProps {
   params: {
     chatId: string;
   };
+}
+async function getChatMessages(chatId: string) {
+  try {
+    const results: string[] = await fetchRedis(
+      `zrange`,
+      `chat:${chatId}:messages`,
+      0,
+      -1
+    );
+    const dbMessages = results.map((message) => {
+      JSON.parse(message) as Message;
+    });
+    const reverseDbMessages = dbMessages.reverse();
+  } catch (error) {
+    notFound();
+  }
 }
 
 const page = async ({ params }: pageProps) => {
@@ -19,8 +37,10 @@ const page = async ({ params }: pageProps) => {
   if (userId1 != user.id && userId2 != user.id) {
     notFound();
   }
+
   const chatPartnerId = user.id === userId1 ? userId2 : userId1;
   const chatPartner = await db.get(`user:${chatPartnerId}`);
+  const initialMessages = await getChatMessages(chatId);
   return <div>hello from chat</div>;
 };
 
--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -1,6 +1,25 @@
-  interface User {
-    name: string;
-    email: string;
-    image: string;
-    id: string;
-  }
+interface User {
+  name: string;
+  email: string;
+  image: string;
+  id: string;
+}
+
+interface Messages {
+  id: string;
+  senderId: string;
+  RecieverId: string;
+  text: string;
+  timestamp: number;
+}
+
+interface Chat {
+  id: string;
+  messages: Messages[];
+}
+
+interface FriendRequest {
+  id: string;
+  senderId: string;
+  RecieverId: string;
+}
