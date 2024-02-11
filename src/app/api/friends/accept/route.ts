512b0ef883a286678b16393b9cd5386639c1f490
Siddharth <siddharth8271skg2@gmail.com>
2024-02-11 19:31:45 +0530

    realtime chat done

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,5 +1,7 @@
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { fetchRedis } from "@/redis/helper";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
@@ -14,7 +16,6 @@ export async function POST(req: Request) {
       return new Response("Unauthorized", { status: 400 });
     }
 
-    //verify if both users are not already friends
     const isAlreadyFriend = await fetchRedis(
       "sismember",
       `user:${session.user.id}:friends`,
@@ -34,9 +35,39 @@ export async function POST(req: Request) {
       });
     }
 
-    db.sadd(`user:${session.user.id}:friends`, idToAdd);
-    db.sadd(`user:${idToAdd}:friends`, session.user.id);
-    db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd);
+    const [userRaw, friendRaw] = (await Promise.all([
+      fetchRedis("get", `user:${session.user.id}`),
+      fetchRedis("get", `user:${idToAdd}`),
+    ])) as [string, string];
+
+    const user = JSON.parse(userRaw) as User;
+    const friend = JSON.parse(friendRaw) as User;
+
+    // notify added user
+
+    await Promise.all([
+      pusherServer.trigger(
+        toPusherKey(`user:${idToAdd}:friends`),
+        "new_friend",
+        user
+      ),
+      pusherServer.trigger(
+        toPusherKey(`user:${session.user.id}:friends`),
+        "new_friend",
+        friend
+      ),
+      db.sadd(`user:${session.user.id}:friends`, idToAdd),
+      db.sadd(`user:${idToAdd}:friends`, session.user.id),
+      db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd),
+    ]);
     return new Response("OK");
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request payload", { status: 422 });
+    }
+
+    return new Response("Invalid request", { status: 400 });
+  }
 }
